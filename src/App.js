c0487f5207c2f87471ae10f632ba59782affb8a1
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-06-24 12:43:53 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Notification from './Notification';
+
+function App() {
+
+  const [connected, setConnected] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState(null);
+  const [notification, setNotification] = useState({ message: '', show: false });
+
+  const connect = async () => {
+    setLoading(true);
+    try {
+      let signer = null;
+      let provider;
+      if (window.ethereum == null) {
+        console.log("MetaMask not installed");
+        const provider = ethers.getDefaultProvider();
+        console.log(provider)
+      } else {
+        const provider = new ethers.BrowserProvider(window.ethereum)
+
+        const network = await provider.getNetwork();
+        const desiredChainId = '0x89';
+        if (network.chainId !== parseInt(desiredChainId)) {
+          try {
+            await window.ethereum.request({
+              method: 'wallet_switchEthereumChain',
+              params: [{ chainId: desiredChainId }],
+            });
+          } catch (switchError) {
+            if (switchError.code === 4902) {
+              try {
+                await window.ethereum.request({
+                  method: 'wallet_addEthereumChain',
+                  params: [{
+                    chainId: desiredChainId,
+                    chainName: 'Polygon',
+                    nativeCurrency: {
+                      name: 'MATIC',
+                      symbol: 'MATIC',
+                      decimals: 18
+                    },
+                    rpcUrls: ['https://polygon-rpc.com/'],
+                    blockExplorerUrls: ['https://polygonscan.com/'],
+                  }],
+                });
+              } catch (addError) {
+                throw addError;
+              }
+            } else {
+              throw switchError;
+            }
+          }
+        }
+        const signer = await provider.getSigner();
+        const message = "hello world!";
+        const sig = await signer.signMessage(message);
+        const address = await signer.getAddress();
+        const balance = await provider.getBalance(address);
+        const parsed = ethers.formatEther(balance);
+        console.log(parsed);
+        const verify = ethers.verifyMessage(message, sig);
+        console.log(verify);
+        setConnected(true);
+
+        const { ethereum } = window;
+        if (ethereum) {
+          const ensProvider = new ethers.InfuraProvider('mainnet');
+          const signer = await provider.getSigner();
+          const address = await signer.getAddress();
+          const displayAddress = address?.substr(0, 6) + "...";
+          const ens = await ensProvider.lookupAddress(address);
+          if (ens !== null) {
+            setName(ens)
+            showNotification("Welcome " + ens);
+          } else {
+            setName(displayAddress)
+            showNotification(displayAddress);
+          }
+        } else {
+          alert('no wallet detected!')
+        }
+      }
+      setLoading(false)
+    } catch (error) {
+      setConnected(false);
+      setLoading(false);
+      showNotification(error.message);
+    }
+  }
+
+  const showNotification = (message) => {
+    setNotification({ message, show: true });
+  };
+
+  const installMetamask = () => {
+    window.open('https://metamask.io/download.html', '_blank');
+  };
+
+  const disconnect = () => {
+    setName(null)
+    setConnected(false)
+  }
+
+  return (
+    <div className="app">
+      <header className="header">
+      <div className={`loading ${loading ? 'show' : ''}`}>
+        <div className="loader"></div>
+      </div>
+        {typeof window.ethereum !== 'undefined' ? (
+          <div>
+          {!connected && (
+            <button className='connect' onClick={connect}>CONNECT</button>
+          )}
+          {connected && (
+            <section>
+            <button className='disconnect' onClick={disconnect}>{name}</button>
+            <h1>Hello World!</h1>
+            </section>
+          )}
+          </div>
+        ) : (
+          <button onClick={installMetamask}>
+            Install Metamask
+          </button>
+        )}
+
+        <Notification
+          message={notification.message}
+          show={notification.show}
+          setShow={(show) => setNotification({ ...notification, show })} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
